5ffb7545ffba2213da5dda5f76387af8d5e15610
shve-axelor <shve.axelor@gmail.com>
2019-08-02 19:01:49 +0530

    Remove controller bindings from module.

--- axelor-gst-app/modules/axelor-gst/src/main/java/com/axelor/gst/module/GstModule.java
+++ axelor-gst-app/modules/axelor-gst/src/main/java/com/axelor/gst/module/GstModule.java
@@ -11,19 +11,11 @@ import com.axelor.gst.service.InvoiceService;
 import com.axelor.gst.service.InvoiceServiceImpl;
 import com.axelor.gst.service.SequenceService;
 import com.axelor.gst.service.SequenceServiceImpl;
-import com.axelor.gst.web.InvoiceController;
-import com.axelor.gst.web.InvoiceLineController;
-import com.axelor.gst.web.ProductController;
-import com.axelor.gst.web.SequenceController;
 
 public class GstModule extends AxelorModule{
 
   @Override
   protected void configure() {
-    bind(SequenceController.class);
-    bind(InvoiceController.class);
-    bind(InvoiceLineController.class);
-    bind(ProductController.class);
     bind(InvoiceService.class).to(InvoiceServiceImpl.class);
     bind(InvoiceLineService.class).to(InvoiceLineServiceImpl.class);
     bind(SequenceService.class).to(SequenceServiceImpl.class);
